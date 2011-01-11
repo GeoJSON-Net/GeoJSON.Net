94d814c72b2cce99d6a02b2a916e71015092a2d3
Jörg Battermann <jb@joergbattermann.com>
2011-01-11 21:55:20 +0100

    Initial checkin

---
+++ GeoJSON.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Jörg Battermann">
+//   Copyright © Jörg Battermann 2011
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GeoJSON.Net")]
+[assembly: AssemblyDescription(".Net types for the GeoJSON 1.0 specs to be used with Json.Net")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jörg Battermann")]
+[assembly: AssemblyProduct("GeoJSON.Net")]
+[assembly: AssemblyCopyright("Copyright © Jörg Battermann 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.0.1")]
+
+[assembly: Guid("ec823c3d-811c-4ea2-ae3c-ed45b7473af4")]
