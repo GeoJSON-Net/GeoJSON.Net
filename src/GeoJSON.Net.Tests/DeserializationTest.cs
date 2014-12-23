dcc5a4a1a9377a5dec6d42b9dfcac153d1453f9a
philo <phil@stormid.com>
2014-12-23 10:58:17 +0000

    Implements multi-polygon serialization

--- src/GeoJSON.Net.Tests/DeserializationTest.cs
+++ src/GeoJSON.Net.Tests/DeserializationTest.cs
@@ -12,6 +12,120 @@ namespace GeoJSON.Net.Tests
     [TestClass]
     public class DeserializationTest
     {
+		[TestMethod]
+		public void MultiPolygonDeserialization()
+		{
+			#region geoJsonText
+
+			var geoJsonText = @"{'coordinates':[[[
+  [
+    -2.6797102391514338,
+    52.959676831105995
+  ],
+  [
+    -2.6769029474483279,
+    52.9608756693609
+  ],
+  [
+    -2.6079763270327119,
+    52.908449372833715
+  ],
+  [
+    -2.5815104708998668,
+    52.891287242948195
+  ],
+  [
+    -2.5851645010668989,
+    52.875476700983896
+  ],
+  [
+    -2.6050779098387191,
+    52.882954723868622
+  ],
+  [
+    -2.6373482332006359,
+    52.875255907042678
+  ],
+  [
+    -2.6932445076063951,
+    52.878791122091066
+  ],
+  [
+    -2.693133462937789,
+    52.89564268523565
+  ],
+  [
+    -2.6548779332193022,
+    52.930592009390175
+  ],
+  [
+    -2.6797102391514338,
+    52.959676831105995
+  ]
+]],[[
+  [
+    -2.69628632041613,
+    52.89610842810761
+  ],
+  [
+    -2.75901233808515,
+    52.8894641454077
+  ],
+  [
+    -2.7663172788742449,
+    52.89938894657412
+  ],
+  [
+    -2.804554822840895,
+    52.90253773227807
+  ],
+  [
+    -2.83848602260174,
+    52.929801009654575
+  ],
+  [
+    -2.838979264607087,
+    52.94013913205788
+  ],
+  [
+    -2.7978187468478741,
+    52.937353122653533
+  ],
+  [
+    -2.772273870352612,
+    52.920394929466184
+  ],
+  [
+    -2.6996509024137052,
+    52.926572918779222
+  ],
+  [
+    -2.69628632041613,
+    52.89610842810761
+  ]
+]]],'type':'MultiPolygon'}";
+
+			#endregion
+
+			var multipolygon = JsonConvert.DeserializeObject<MultiPolygon>(geoJsonText,
+				new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+
+			Assert.IsNotNull(multipolygon);
+			Assert.IsNotNull(multipolygon.Coordinates);
+			Assert.IsTrue(multipolygon.Coordinates.Count == 2);
+			Assert.IsTrue(multipolygon.Coordinates[0].Coordinates.Count == 1);
+			Assert.IsTrue(multipolygon.Coordinates[0].Coordinates[0].Coordinates.Count == 11);
+			var firstPolygon = multipolygon.Coordinates.First();
+			var firstPoint = firstPolygon.Coordinates[0].Coordinates[0] as GeographicPosition;
+			Assert.IsTrue(Math.Abs(firstPoint.Latitude - 52.959676831105995) < 0.0001);
+			Assert.IsTrue(Math.Abs(firstPoint.Longitude - -2.6797102391514338) < 0.0001);
+
+			var lastPolygon = multipolygon.Coordinates.Last();
+			var lastPoint = lastPolygon.Coordinates.Last().Coordinates.Last() as GeographicPosition;
+			Assert.IsTrue(Math.Abs(lastPoint.Latitude - 52.89610842810761) < 0.0001);
+			Assert.IsTrue(Math.Abs(lastPoint.Longitude - -2.69628632041613) < 0.0001);
+		}
+
         [TestMethod]
         public void PolygonDeserialization()
         {
