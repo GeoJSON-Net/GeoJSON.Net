61b1c0a5e967c77cf4033ccd5df51731a4f248fe
hanssens <github@hanssens.com>
2016-11-14 11:05:53 +0100

    Fixed dynamic filepaths to test files (*.json) - Test .json files are now included in output build, for the test project - dotnet-core does some different magic with the output assembly names, which is also corrected now

--- src/test/GeoJSON.Net.Tests/TestBase.cs
+++ src/test/GeoJSON.Net.Tests/TestBase.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -7,23 +8,24 @@ namespace GeoJSON.Net.Tests
 {
     public abstract class TestBase
     {
-        private static readonly string AssemblyName = typeof(TestBase).Assembly.GetName().Name;
+        private static readonly string AssemblyName = typeof(TestBase).Name;
 
         public static string AssemblyDirectory
         {
             get
-            {
-                string codeBase = typeof(TestBase).Assembly.CodeBase;
+            {              
+                var codeBase = System.AppContext.BaseDirectory;
                 UriBuilder uri = new UriBuilder(codeBase);
                 string path = Uri.UnescapeDataString(uri.Path);
-                return Path.GetDirectoryName(path);
+                return path;
+                
             }
         }
 
         protected string GetExpectedJson([CallerMemberName] string name = null)
         {
             var type = GetType().Name;
-            var projectFolder = GetType().Namespace.Substring(AssemblyName.Length + 1);
+            var projectFolder = GetType().Namespace.Split('.').Last();
             var path = Path.Combine(AssemblyDirectory, @".\", projectFolder, type + "_" + name + ".json");
 
             if (!File.Exists(path))
