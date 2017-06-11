c045cc9481ee9e2115f22183877088005afea108
Michael Helmling <michaelhelmling@posteo.de>
2017-06-11 15:49:34 +0200

    Add generic Feature<TGeometry, TProps> class

--- src/GeoJSON.Net/Feature/Feature.cs
+++ src/GeoJSON.Net/Feature/Feature.cs
@@ -13,6 +13,88 @@ using System;
 namespace GeoJSON.Net.Feature
 {
     /// <summary>
+    /// A GeoJSON Feature Object; generic version for strongly typed <see cref="Geometry"/>
+    /// and <see cref="Properties"/>
+    /// </summary>
+    /// <remarks>
+    /// See https://tools.ietf.org/html/rfc7946#section-3.2
+    /// </remarks>
+    public class Feature<TGeometry, TProps> : GeoJSONObject, IEquatable<Feature<TGeometry, TProps>>
+        where TGeometry : IGeometryObject
+    {
+        [JsonConstructor]
+        public Feature(TGeometry geometry, TProps properties, string id = null)
+        {
+            Geometry = geometry;
+            Properties = properties;
+            Id = id;
+        }
+        
+        [JsonProperty(PropertyName = "id", NullValueHandling = NullValueHandling.Ignore)]
+        public string Id { get; }
+        
+        [JsonProperty(PropertyName = "geometry", Required = Required.AllowNull)]
+        [JsonConverter(typeof(GeometryConverter))]
+        public TGeometry Geometry { get; }
+        
+        [JsonProperty(PropertyName = "properties", Required = Required.AllowNull)]
+        public TProps Properties { get;}
+        
+        /// <summary>
+        /// Equality comparer.
+        /// </summary>
+        /// <remarks>
+        /// In contrast to <see cref="Feature.Equals(Feature)"/>, this implementation returns true only
+        /// if <see cref="Id"/> and <see cref="Properties"/> are also equal. See
+        /// <a href="https://github.com/GeoJSON-Net/GeoJSON.Net/issues/80">#80</a> for discussion. The rationale
+        /// here is that a user explicitly specifying the property type most probably cares about the properties
+        /// equality.
+        /// </remarks>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(Feature<TGeometry, TProps> other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return base.Equals(other)
+                   && string.Equals(Id, other.Id)
+                   && EqualityComparer<TGeometry>.Default.Equals(Geometry, other.Geometry)
+                   && EqualityComparer<TProps>.Default.Equals(Properties, other.Properties);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Feature<TGeometry, TProps>) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = base.GetHashCode();
+                hashCode = (hashCode * 397) ^ (Id != null ? Id.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ EqualityComparer<TGeometry>.Default.GetHashCode(Geometry);
+                hashCode = (hashCode * 397) ^ EqualityComparer<TProps>.Default.GetHashCode(Properties);
+                return hashCode;
+            }
+        }
+
+        public static bool operator ==(Feature<TGeometry, TProps> left, Feature<TGeometry, TProps> right)
+        {
+            return object.Equals(left, right);
+        }
+
+        public static bool operator !=(Feature<TGeometry, TProps> left, Feature<TGeometry, TProps> right)
+        {
+            return !object.Equals(left, right);
+        }
+    }
+    
+    
+    /// <summary>
     /// A GeoJSON Feature Object.
     /// </summary>
     /// <remarks>
