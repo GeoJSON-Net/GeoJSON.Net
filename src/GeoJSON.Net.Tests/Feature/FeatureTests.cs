a279990351ddcdce760934f8c56bbcd1addfbb1c
Michael Helmling <michaelhelmling@posteo.de>
2017-06-17 21:01:22 +0100

    Add test for deserializing typed feature

--- src/GeoJSON.Net.Tests/Feature/FeatureTests.cs
+++ src/GeoJSON.Net.Tests/Feature/FeatureTests.cs
@@ -29,6 +29,31 @@ namespace GeoJSON.Net.Tests.Feature
             Assert.AreEqual(feature.Geometry.Type, GeoJSONObjectType.Point);
         }
 
+        private class TypedFeatureProps
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            [JsonProperty("value")]
+            public double Value { get; set; }
+        }
+
+        [Test]
+        public void Can_Deserialize_Typed_Point_Feature()
+        {
+            var json = GetExpectedJson();
+            var feature = JsonConvert.DeserializeObject<Net.Feature.Feature<Point, TypedFeatureProps>>(json);
+
+            Assert.IsNotNull(feature);
+
+            Assert.IsNotNull(feature.Properties);
+            Assert.AreEqual(feature.Properties.Name, "Dinagat Islands");
+            Assert.AreEqual(feature.Properties.Value, 4.2);            
+
+            Assert.AreEqual(feature.Id, "test-id");
+
+            Assert.AreEqual(feature.Geometry.Type, GeoJSONObjectType.Point);
+        }
+
         [Test]
         public void Can_Serialize_LineString_Feature()
         {
