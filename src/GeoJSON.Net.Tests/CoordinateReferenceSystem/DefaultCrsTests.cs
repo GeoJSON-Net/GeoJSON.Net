e466e84636ddde196ede8ddf034d28b81d8f65cb
matt-lethargic <mattlethargic@gmail.com>
2017-08-08 16:00:36 +0100

    Adding some tests

--- src/GeoJSON.Net.Tests/CoordinateReferenceSystem/DefaultCrsTests.cs
+++ src/GeoJSON.Net.Tests/CoordinateReferenceSystem/DefaultCrsTests.cs
@@ -29,5 +29,41 @@ namespace GeoJSON.Net.Tests.CoordinateReferenceSystem
             Assert.IsNull(point.CRS);
         }
 
+        [Test]
+        public void Can_Deserialize_CRS_issue_89()
+        {
+            var json = "{\"coordinates\": [ 90.65464646, 53.2455662, 200.4567 ], \"type\": \"Point\", \"crs\": { \"type\": \"name\", \"properties\": { \"name\": \"urn:ogc:def:crs:OGC:1.3:CRS84\" }}}";
+
+            var point = JsonConvert.DeserializeObject<Point>(json);
+
+            Assert.IsNotNull(point.CRS);
+            Assert.AreEqual(CRSType.Name, point.CRS.Type);
+        }
+
+        [Test]
+        public void Can_Serialize_CRS_issue_89()
+        {
+            var expected =
+                "{\"type\":\"Point\",\"coordinates\":[34.56,12.34],\"crs\":{\"properties\":{\"name\":\"TEST NAME\"},\"type\":\"name\"}}";
+            var point = new Point(new Position(12.34, 34.56)) { CRS = new NamedCRS("TEST NAME") };
+
+            var json = JsonConvert.SerializeObject(point);
+
+            Assert.IsNotNull(json);
+            Assert.AreEqual(expected, json);
+        }
+
+        [Test]
+        public void Can_Serialize_DefaultCRS_issue_89()
+        {
+            var expected =
+                "{\"type\":\"Point\",\"coordinates\":[34.56,12.34],\"crs\":{\"properties\":{\"name\":\"urn:ogc:def:crs:OGC::CRS84\"},\"type\":\"name\"}}";
+            var point = new Point(new Position(12.34, 34.56)) { CRS = new NamedCRS("urn:ogc:def:crs:OGC::CRS84") };
+
+            var json = JsonConvert.SerializeObject(point);
+
+            Assert.IsNotNull(json);
+            Assert.AreEqual(expected, json);
+        }
     }
 }
